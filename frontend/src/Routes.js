a6f6a667389b3795e45eefda4766b0ccb250a340
sothulthorn <thornsothul@gmail.com>
2025-03-04 21:18:55 +0900

    Add Signup page

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './core/Home';
+import PrivateRoutes from './auth/helper/PrivateRoutes';
+import Signup from './user/Signup';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup} />
+        {/* <PrivateRoutes path="/user/dashboard" exact component={} /> */}
       </Switch>
     </BrowserRouter>
   );
